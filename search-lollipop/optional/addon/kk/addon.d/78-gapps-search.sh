f31d9cab91241aa81d96d64a2070c200da594521
syphyr <syphyr@gmail.com>
2017-04-10 21:58:14 +0200

    Add Google Search

---
+++ search-lollipop/optional/addon/kk/addon.d/78-gapps-search.sh
@@ -0,0 +1,44 @@
+#!/sbin/sh
+# 
+# /system/addon.d/78-gapps-search.sh
+#
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+priv-app/Velvet.apk
+lib/libcronet.so
+lib/libframesequence.so
+lib/libgoogle_speech_jni.so
+lib/libgoogle_speech_micro_jni.so
+lib/libnativecrashreporter.so
+lib/liboffline_actions_jni.so
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/$FILE
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/$FILE $R
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    rm -f /system/app/QuickSearchBox.apk
+  ;;
+esac
---
+++ search-lollipop/scripts/install-core-0.sh
@@ -0,0 +1,43 @@
+#!/sbin/sh
+
+APKFILE="Velvet.apk"
+APKPATH="/tmp/apkbin/priv-app/Velvet"
+SYSPATH="priv-app"
+
+LCD=$(grep ro.sf.lcd_density /system/build.prop | cut -d "=" -f 2);
+
+if [ ! "$LCD" == 160 ] && [ ! "$LCD" == 240 ] && [ ! "$LCD" == 320 ]; then
+  echo "Installing default."
+  if grep ro.build.version.release /system/build.prop | grep 4.4.; then
+    echo kk
+    cd "$APKPATH"
+    /tmp/unzip -o "$APKFILE" lib/* -d ./
+    mkdir -p ./lib/arm
+    mv ./lib/armeabi-v7a/* ./lib/arm/
+    rmdir ./lib/armeabi-v7a
+    /tmp/zip "$APKFILE" -d ./lib/armeabi-v7a/*
+    /tmp/zipalign -f 4 "$APKFILE" aligned.apk
+    mv aligned.apk "$APKFILE"
+    cp -a /tmp/addon/kk/* /system/
+    cp -f "$APKPATH"/"$APKFILE" /system/"$SYSPATH"/
+    cp -a "$APKPATH"/lib/arm/* /system/lib/
+  elif grep ro.build.version.release /system/build.prop | grep -qE '5.0.|5.1.'; then
+    echo lp
+    cd "$APKPATH"
+    /tmp/unzip -o "$APKFILE" lib/* -d ./
+    mkdir -p ./lib/arm
+    mv ./lib/armeabi-v7a/* ./lib/arm/
+    rmdir ./lib/armeabi-v7a
+    /tmp/zip "$APKFILE" -d ./lib/armeabi-v7a/*
+    /tmp/zipalign -f 4 "$APKFILE" aligned.apk
+    mv aligned.apk "$APKFILE"
+    cp -a /tmp/addon/lp/* /system/
+    cp -a /tmp/apkbin/* /system/
+  else
+    echo mm
+    cp -a /tmp/addon/lp/* /system/
+    cp -a /tmp/apkbin/* /system/
+    sed -i '/Velvet\/lib/ d' /system/addon.d/90-gapps-search.sh
+  fi
+fi
+rm -f "$APKPATH"/"$APKFILE"
