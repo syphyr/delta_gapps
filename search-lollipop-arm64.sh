4845e125d84e1071853dde3f7f4f1ae4208c46fe
syphyr <syphyr@gmail.com>
2017-04-12 21:02:49 +0200

    Add Google Search arm64

---
+++ search-lollipop-arm64.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+APKLIST=$(find search-lollipop-arm64/optional/apkbin -name com.*.apk | sort)
+FILENAME=Velvet.apk
+INDIR=search-lollipop-arm64
+NAME=gapps-search
+VER=arm64-lmn
+
+DATE=$(date +%F-%H-%M)
+BASEDIR=$(pwd)
+
+echo "" >> build.log
+echo "Updating "$INDIR" on $DATE for lollipop, marshmallow, and nougat" >> build.log
+echo "Google Search add-on for 5.0.2+ (arm64) (replaces stock quicsearchbox)" >> build.log
+
+for FILEPATH in $APKLIST ; do
+
+DIR=$(dirname "${FILEPATH}")
+FILE=${FILEPATH##*/}
+NOEXT=${FILE%\.*}
+
+VERSION=$(echo "$FILE" | cut -d "_" -f 2)
+APIVER=$(echo "$FILE" | cut -d "_" -f 3)
+
+cd "$BASEDIR"
+cd "$DIR"
+
+echo "Extracting libraries from apk"
+if [ -e "$FILENAME" ]; then
+  unzip -o "$FILENAME" lib/* -d ./
+  mv lib lib.old
+fi
+unzip -o "$FILE" lib/* -d ./
+
+if [ ! -d lib/arm64-v8a ] ; then
+  echo "Libraries are not for arm64"
+else
+
+if [ -d lib/arm64-v8a ] ; then
+  mkdir ./lib/arm64
+  cp -a ./lib/arm64-v8a/* ./lib/arm64/
+  #echo "Deleting lib directory inside apk file"
+  zip "$FILE" -d ./lib/arm64-v8a/*
+  #echo "Inserting decompressed libraries inside apk file"
+  zip -r -D -Z store -b ./ "$FILE" ./lib/arm64-v8a/
+  rm -rf  ./lib/arm64-v8a
+fi
+
+if [ -d lib.old/arm64-v8a ] ; then
+  mkdir ./lib.old/arm64
+  cp -a ./lib.old/arm64-v8a/* ./lib.old/arm64/
+  rm -rf  ./lib.old/arm64-v8a
+fi
+
+if [ -d lib/armeabi-v7a ] ; then
+  mkdir ./lib/arm
+  cp -a ./lib/armeabi-v7a/* ./lib/arm/
+  #echo "Deleting lib directory inside apk file"
+  zip "$FILE" -d ./lib/armeabi-v7a/*
+  #echo "Inserting decompressed libraries inside apk file"
+  zip -r -D -Z store -b ./ "$FILE" ./lib/armeabi-v7a/
+  rm -rf  ./lib/armeabi-v7a
+fi
+
+if [ -d lib.old/armeabi-v7a ] ; then
+  mkdir ./lib.old/arm
+  cp -a ./lib.old/armeabi-v7a/* ./lib.old/arm/
+  rm -rf  ./lib.old/armeabi-v7a
+fi
+
+echo "Aligning apk and libraries for 32bit systems"
+zipalign -f -p 4 "$FILE" "$NOEXT"-aligned.apk
+rm "$FILE"
+mv "$NOEXT"-aligned.apk "$FILENAME"
+
+echo "Libraries aligned."
+
+fi
+
+echo "Version: $VERSION" >> "$BASEDIR"/build.log
+echo "API: $APIVER" >> "$BASEDIR"/build.log
+
+if [ -d lib ] && [ -d lib.old ]; then
+  diff -rq lib.old lib | grep Only >> "$BASEDIR"/build.log
+fi
+
+echo "Removing extracted files."
+echo "" >> "$BASEDIR"/build.log
+
+if [ -d lib ]; then
+  rm -rf lib
+fi
+
+if [ -d lib.old ]; then
+  rm -rf lib.old
+fi
+
+done
+
+cd "$BASEDIR"
+./makezipsign.sh "$INDIR" "$NAME" "$VER"
---
+++ search-lollipop-arm64/scripts/install-core-0.sh
@@ -0,0 +1,32 @@
+#!/sbin/sh
+
+APKFILE="Velvet.apk"
+APKPATH="/tmp/apkbin/priv-app/Velvet"
+SYSPATH="priv-app"
+
+echo "Installing default."
+if grep ro.build.version.release /system/build.prop | grep -qE '5.0.|5.1.'; then
+  echo lp
+  cd "$APKPATH"
+  /tmp/unzip -o "$APKFILE" lib/* -d ./
+  mkdir -p ./lib/arm64
+  mv ./lib/arm64-v8a/* ./lib/arm64/
+  rmdir ./lib/arm64-v8a
+  /tmp/zip "$APKFILE" -d ./lib/arm64-v8a/*
+  /tmp/zipalign -f 4 "$APKFILE" aligned.apk
+  mv aligned.apk "$APKFILE"
+  cp -a /tmp/addon/lp/* /system/
+  cp -a /tmp/apkbin/* /system/
+else
+  echo mm
+  cp -a /tmp/addon/lp/* /system/
+  cp -a /tmp/apkbin/* /system/
+  sed -i '/Velvet\/lib/ d' /system/addon.d/90-gapps-search.sh
+fi
+
+rm -rf /tmp/apkbin
+rm -rf /tmp/addon
+rm -f /tmp/busybox
+rm -f /tmp/unzip
+rm -f /tmp/zip
+rm -f /tmp/zipalign
