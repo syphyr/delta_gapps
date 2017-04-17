ef06d9ce1371246b1e201969728971e4892483f5
syphyr <syphyr@gmail.com>
2017-04-17 02:37:46 +0200

    Add Google Messenger

---
+++ messenger-arm/optional/addon/lp/addon.d/95-gapps-messenger.sh
@@ -0,0 +1,44 @@
+#!/sbin/sh
+# 
+# /system/addon.d/95-gapps-messenger.sh
+#
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+priv-app/GoogleMessenger/GoogleMessenger.apk
+priv-app/GoogleMessenger/lib/arm/libframesequence.so
+priv-app/GoogleMessenger/lib/arm/libgiftranscode.so
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/$FILE
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/$FILE $R
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    rm -rf /system/app/messaging
+    rm -rf /system/priv-app/Mms
+    chmod 755 /system/priv-app/GoogleMessenger
+    chmod 755 /system/priv-app/GoogleMessenger/lib
+    chmod 755 /system/priv-app/GoogleMessenger/lib/arm
+  ;;
+esac
