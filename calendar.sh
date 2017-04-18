3bb7b55b06895f010aa0cd34c8e911ba867e64b0
syphyr <syphyr@gmail.com>
2017-04-18 21:54:51 +0200

    Add Calendar

---
+++ calendar.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+FILEPATH=$(find calendar/optional/apkbin -name com.*.apk | sort)
+FILENAME=CalendarGooglePrebuilt.apk
+INDIR=calendar
+NAME=gapps-calendar
+VER=arm-arm64-klmn
+
+DATE=$(date +%F-%H-%M)
+BASEDIR=$(pwd)
+
+function tout {
+  tee /dev/tty >> "$BASEDIR"/build.log
+}
+
+echo "" | tout
+echo "Updating "$INDIR" on $DATE for kitkat, lollipop, marshmallow, and nougat" | tout
+echo "Google Calendar add-on for 4.4.4+ (arm/arm64) (replaces stock calendar)" | tout
+echo "" | tout
+
+if [ ! "$FILEPATH" == "" ]; then
+  DIR=$(dirname "${FILEPATH}")
+  FILE=${FILEPATH##*/}
+  NOEXT=${FILE%\.*}
+
+  cd "$DIR"
+
+  if [ ! "$FILE" == "" ]; then
+    rm "$FILENAME"
+    mv "$FILE" "$FILENAME"
+
+    VERSION=$(echo "$FILE" | cut -d "_" -f 2)
+    APIVER=$(echo "$FILE" | cut -d "_" -f 3)
+
+    cd "$BASEDIR"
+
+    echo "Version: $VERSION" | tout
+    echo "API: $APIVER" | tout
+    echo "" | tout
+  fi
+fi
+
+./makezipsign.sh "$INDIR" "$NAME" "$VER"
