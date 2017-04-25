0cafe75c7282f02af366a24cdca5221b25abdf9d
syphyr <syphyr@gmail.com>
2017-04-25 23:48:43 +0200

    Add Play Books

---
+++ books/scripts/install-optional.sh
@@ -0,0 +1,21 @@
+#!/sbin/sh
+
+APKFILE="GoogleBooks.apk"
+APKPATH="/tmp/apkbin/app/GoogleBooks"
+SYSPATH="app"
+
+if grep ro.build.version.release /system/build.prop | grep 4.4.; then
+  echo kk
+  cp -a /tmp/addon/kk/* /system/
+  cp -f "$APKPATH"/"$APKFILE" /system/"$SYSPATH"/
+elif grep ro.build.version.release /system/build.prop | grep -qE '5.0.|5.1.'; then
+  echo lp
+  cp -a /tmp/addon/lp/* /system/
+  cp -a /tmp/apkbin/* /system/
+else
+  echo mm
+  cp -a /tmp/addon/lp/* /system/
+  cp -a /tmp/apkbin/* /system/
+fi
+rm -rf /tmp/addon
+rm -rf /tmp/apkbin
