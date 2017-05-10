512655d3d1cce6151f38bb510025211a20c178bc
syphyr <syphyr@gmail.com>
2017-05-10 23:41:34 +0200

    Hangouts arm64

---
+++ hangouts-arm64/scripts/install-core-5.sh
@@ -0,0 +1,33 @@
+#!/sbin/sh
+
+APKFILE="Hangouts.apk"
+APKPATH="/tmp/apkbin/app/Hangouts"
+
+LCD=$(grep ro.sf.lcd_density /system/build.prop | cut -d "=" -f 2);
+
+if [ "$LCD" == 560 ] || [ "$LCD" == 640 ]; then
+  echo "LCD 560,640 detected."
+  if grep ro.build.version.release /system/build.prop | grep -qE '5.0.|5.1.'; then
+    echo lp
+    cd "$APKPATH"
+    /tmp/unzip -o "$APKFILE" lib/* -d ./
+    mkdir -p ./lib/arm64
+    mv ./lib/arm64-v8a/* ./lib/arm64/
+    rmdir ./lib/arm64-v8a
+    /tmp/zip "$APKFILE" -d ./lib/arm64-v8a/*
+    /tmp/zipalign -f 4 "$APKFILE" aligned.apk
+    mv aligned.apk "$APKFILE"
+    cp -a /tmp/addon/lp/* /system/
+    cp -a /tmp/apkbin/app/* /system/priv-app/
+  else
+    echo mm
+    cp -a /tmp/addon/mm/* /system/
+    cp -a /tmp/apkbin/* /system/
+  fi
+fi
+rm -rf /tmp/addon
+rm -rf /tmp/apkbin
+rm -f /tmp/busybox
+rm -f /tmp/unzip
+rm -f /tmp/zip
+rm -f /tmp/zipalign
