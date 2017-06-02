2702308081503aca12dc5fae7ce6ccbb3edba1f5
syphyr <syphyr@gmail.com>
2017-06-03 01:41:00 +0200

    Add Snapseed (arm)

---
+++ snapseed-arm/scripts/install-core-2.sh
@@ -0,0 +1,27 @@
+#!/sbin/sh
+
+APKFILE="Snapseed.apk"
+APKPATH="/tmp/apkbin/app/Snapseed"
+
+LCD=$(grep ro.sf.lcd_density /system/build.prop | cut -d "=" -f 2);
+
+if [ "$LCD" == 160 ]; then
+  echo "LCD 160 detected."
+  if grep ro.build.version.release /system/build.prop | grep -qE '5.0.|5.1.'; then
+    echo lp
+    cd "$APKPATH"
+    /tmp/unzip -o "$APKFILE" lib/* -d ./
+    mkdir -p ./lib/arm
+    mv ./lib/armeabi-v7a/* ./lib/arm/
+    rmdir ./lib/armeabi-v7a
+    /tmp/zip "$APKFILE" -d ./lib/armeabi-v7a/*
+    /tmp/zipalign -f 4 "$APKFILE" aligned.apk
+    mv aligned.apk "$APKFILE"
+    cp -a /tmp/apkbin/* /system/
+  else
+    echo mm
+    cp -a /tmp/apkbin/* /system/
+    sed -i '/Snapseed\/lib/ d' /system/addon.d/94-gapps-snapseed.sh
+  fi
+fi
+rm -f "$APKPATH"/"$APKFILE"
