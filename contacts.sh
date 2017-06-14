aa85ab09c8e8f57259b18189ddba4a8ba2ba7b51
syphyr <syphyr@gmail.com>
2017-06-14 20:34:42 +0200

    Add Google Contacts

---
+++ contacts.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+FILEPATH=$(find contacts/system/priv-app/GoogleContacts -name com.*.apk | sort)
+FILENAME=GoogleContacts.apk
+INDIR=contacts
+NAME=gapps-contacts
+VER=arm-arm64-lmn
+
+DATE=$(date +%F-%H-%M)
+BASEDIR=$(pwd)
+
+function tout {
+  tee /dev/tty >> "$BASEDIR"/build.log
+}
+
+echo "" | tout
+echo "Updating "$INDIR" on $DATE for lollipop, marshmallow, and nougat" | tout
+echo "Google Contacts add-on for 5.0.2+ (arm/arm64) (replaces stock contacts)" | tout
+echo "" | tout
+
+if [ ! "$FILEPATH" == "" ]; then
+  DIR=$(dirname "${FILEPATH}")
+  FILE=${FILEPATH##*/}
+  NOEXT=${FILE%\.*}
+
+  cd "$DIR"
+
+  if [ ! "$FILE" == "" ]; then
+    rm "$FILENAME"
+    mv "$FILE" "$FILENAME"
+
+    VERSION=${FILE%_min*}
+    VERSION=${VERSION#*_}
+    APIVER=$(echo ${FILE#*_min} | cut -d "_" -f 1)
+
+    cd "$BASEDIR"
+
+    echo "Version: $VERSION" | tout
+    echo "minAPI/DPI: $APIVER" | tout
+    echo "" | tout
+  fi
+fi
+
+./makezipsign.sh "$INDIR" "$NAME" "$VER"
