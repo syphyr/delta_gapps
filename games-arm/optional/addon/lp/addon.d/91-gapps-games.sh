f8a437735afed85781995e519db1e2c995d7020a
syphyr <syphyr@gmail.com>
2017-07-14 01:16:49 +0200

    Add Play Games arm/arm64

---
+++ games-arm/optional/addon/lp/addon.d/91-gapps-games.sh
@@ -0,0 +1,41 @@
+#!/sbin/sh
+# 
+# /system/addon.d/91-gapps-games.sh
+#
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+app/PlayGames/PlayGames.apk
+app/PlayGames/lib/arm/libgames_rtmp_jni.so
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/$FILE
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/$FILE $R
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    chmod 755 /system/app/PlayGames
+    chmod 755 /system/app/PlayGames/lib
+    chmod 755 /system/app/PlayGames/lib/arm
+  ;;
+esac
