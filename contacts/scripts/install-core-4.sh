28759cc8aaf76cf7879a5e1e3ec314accd0fadd4
syphyr <syphyr@gmail.com>
2017-08-07 23:12:29 +0200

    Add 420 dpi to Contacts

--- contacts/scripts/install-core-4.sh
+++ contacts/scripts/install-core-4.sh
@@ -5,7 +5,7 @@ APKPATH="/tmp/apkbin/priv-app/GoogleContacts"
 
 LCD=$(grep ro.sf.lcd_density /system/build.prop | cut -d "=" -f 2);
 
-if [ "$LCD" == 400 ] || [ "$LCD" == 480 ]; then
+if [ "$LCD" == 400 ] || [ "$LCD" == 420 ] || [ "$LCD" == 480 ]; then
   echo "LCD 400-480 detected."
   if grep ro.build.version.release /system/build.prop | grep -qE '5.0.|5.1.'; then
     echo lp
