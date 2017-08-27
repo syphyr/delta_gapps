9b023cbd6fa86e07f09a90997d27c60fb07125bd
syphyr <syphyr@gmail.com>
2017-08-28 00:54:15 +0200

    Add Google Maps arm64

---
+++ maps-arm64/optional/addon/lp/addon.d/80-gapps-maps.sh
@@ -0,0 +1,42 @@
+#!/sbin/sh
+# 
+# /system/addon.d/80-gapps-maps.sh
+#
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+app/GoogleMaps/GoogleMaps.apk
+app/GoogleMaps/lib/arm64/libcronet.61.0.3163.27.so
+app/GoogleMaps/lib/arm64/libgmm-jni.so
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/$FILE
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/$FILE $R
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    chmod 755 /system/app/GoogleMaps
+    chmod 755 /system/app/GoogleMaps/lib
+    chmod 755 /system/app/GoogleMaps/lib/arm64
+  ;;
+esac
---
+++ maps-arm64/scripts/install-core-0.sh
@@ -0,0 +1,30 @@
+#!/sbin/sh
+
+APKFILE="GoogleMaps.apk"
+APKPATH="/tmp/apkbin/app/GoogleMaps"
+SYSPATH="app"
+
+LCD=$(grep ro.sf.lcd_density /system/build.prop | cut -d "=" -f 2);
+
+if [ ! "$LCD" == 120 ] && [ ! "$LCD" == 160 ] && [ ! "$LCD" == 213 ] && [ ! "$LCD" == 240 ] && [ ! "$LCD" == 320 ] && [ ! "$LCD" == 400 ] && [ ! "$LCD" == 480 ] && [ ! "$LCD" == 560 ] && [ ! "$LCD" == 640 ]; then
+  echo "Installing default."
+  if grep ro.build.version.release /system/build.prop | grep -qE '5.0.|5.1.'; then
+    echo lp
+    cd "$APKPATH"
+    /tmp/unzip -o "$APKFILE" lib/* -d ./
+    mkdir -p ./lib/arm64
+    mv ./lib/arm64-v8a/* ./lib/arm64/
+    rmdir ./lib/arm64-v8a
+    /tmp/zip "$APKFILE" -d ./lib/arm64-v8a/*
+    /tmp/zipalign -f 4 "$APKFILE" aligned.apk
+    mv aligned.apk "$APKFILE"
+    cp -a /tmp/addon/lp/* /system/
+    cp -a /tmp/apkbin/* /system/
+  else
+    echo mm
+    cp -a /tmp/addon/lp/* /system/
+    cp -a /tmp/apkbin/* /system/
+    sed -i '/GoogleMaps\/lib/ d' /system/addon.d/80-gapps-maps.sh
+  fi
+fi
+rm -f "$APKPATH"/"$APKFILE"
