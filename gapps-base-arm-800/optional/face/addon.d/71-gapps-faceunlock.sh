c629cbbf6d8ad105cb0da14a9acbe5725e1c8c3e
syphyr <syphyr@gmail.com>
2017-09-05 21:58:21 +0200

    Add Oreo Base gapps for ARM

---
+++ gapps-base-arm-800/optional/face/addon.d/71-gapps-faceunlock.sh
@@ -0,0 +1,46 @@
+#!/sbin/sh
+# 
+# /system/addon.d/71-gapps-faceunlock.sh
+#
+
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+app/FaceLock/FaceLock.apk
+lib/libfacenet.so
+lib/libfilterpack_facedetect.so
+lib/libfrsdk.so
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/$FILE
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/$FILE $R
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    mkdir -p /system/app/FaceLock/lib/arm
+    chmod 755 /system/app/FaceLock
+    chmod 755 /system/app/FaceLock/lib
+    chmod 755 /system/app/FaceLock/lib/arm
+    ln -s /system/lib/libfacenet.so /system/app/FaceLock/lib/arm/libfacenet.so
+  ;;
+esac
---
+++ gapps-base-arm-800/scripts/install-optional.sh
@@ -0,0 +1,72 @@
+#!/sbin/sh
+
+good_ffc_device() {
+  if [ -f /sdcard/.forcefacelock ]; then
+    return 0
+  elif [ -f /sdcard/.removefacelock ]; then
+    return 1
+  elif cat /proc/cpuinfo | grep -qE 'Victory|herring|sun4i'; then
+    return 1
+  elif [ -e /system/etc/permissions/android.hardware.camera.front.xml ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+if [ -f /sdcard/.addconfigupdater ]; then
+  echo "Installing ConfigUpdater application."
+  cp -a /tmp/config/* /system/
+else
+  sed -i '/ConfigUpdater/ d' /system/addon.d/70-gapps.sh
+fi
+rm -rf /tmp/config
+
+if [ -e /system/etc/permissions/android.hardware.telephony.gsm.xml ] || [ -e /system/etc/permissions/android.hardware.telephony.cdma.xml ]; then
+  echo "Installing dialer framework."
+  cp -a /tmp/dialer/* /system/
+else
+  sed -i '/dialer/ d' /system/addon.d/70-gapps.sh
+fi
+rm -rf /tmp/dialer
+
+if good_ffc_device; then
+  echo "Installing FaceLock."
+  cp -a /tmp/face/* /system/
+  mkdir -p /system/app/FaceLock/lib/arm
+  ln -s /system/lib/libfacenet.so /system/app/FaceLock/lib/arm/libfacenet.so
+else
+  rm -f /system/addon.d/71-gapps-faceunlock.sh
+  rm -f /system/lib/libfacenet.so
+  rm -f /system/lib/libfilterpack_facedetect.so
+  rm -f /system/lib/libfrsdk.so
+fi
+rm -rf /tmp/face
+
+TYPE=$(grep ro.build.characteristics /system/build.prop | grep tablet);
+
+if [ -f /sdcard/.removesetupwizard ]; then
+  sed -i '/Provision/ d' /system/addon.d/70-gapps.sh
+  sed -i '/SetupWizard/ d' /system/addon.d/70-gapps.sh
+elif [ -f /sdcard/.addsetupwizard ]; then
+  sed -i '/Provision/ d' /system/addon.d/70-gapps.sh
+  mkdir /system/priv-app/SetupWizard
+  if [ ! "$TYPE" == "" ]; then
+    echo "Tablet detected."
+    cp -f /tmp/setup/priv-app/SetupWizard/SetupWizard.apk /system/priv-app/SetupWizard/
+  else
+    echo "Phone detected."
+    cp -f /tmp/setup/priv-app/SetupWizard/SetupWizard.apk /system/priv-app/SetupWizard/
+  fi
+else
+  rm -rf /system/app/Provision
+  mkdir /system/priv-app/SetupWizard
+  if [ ! "$TYPE" == "" ]; then
+    echo "Tablet detected."
+    cp -f /tmp/setup/priv-app/SetupWizard/SetupWizard.apk /system/priv-app/SetupWizard/
+  else
+    echo "Phone detected."
+    cp -f /tmp/setup/priv-app/SetupWizard/SetupWizard.apk /system/priv-app/SetupWizard/
+  fi
+fi
+rm -rf /tmp/setup
