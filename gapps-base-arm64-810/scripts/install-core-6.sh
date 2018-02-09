f6cc76998d5c05572407a334fe2c40c2912b7b19
syphyr <syphyr@gmail.com>
2018-02-10 00:11:55 +0100

    Add 320 and 480 dpi gms variants to 8.1.0 Base

--- gapps-base-arm64-810/scripts/install-core-6.sh
+++ gapps-base-arm64-810/scripts/install-core-6.sh
@@ -5,8 +5,8 @@ APKPATH="/tmp/gms/priv-app/PrebuiltGmsCore"
 
 LCD=$(grep ro.sf.lcd_density /system/build.prop | cut -d "=" -f 2);
 
-if [ ! "$LCD" == 320 ] && [ ! "$LCD" == 480 ]; then
-  echo "Installing default."
+if [ "$LCD" == 320 ]; then
+  echo "LCD 320 detected."
   cp -a /tmp/gms/* /system/
 fi
 rm -f "$APKPATH"/"$APKFILE"
